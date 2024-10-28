504a190f318b11a24ee454bf4df9dfbf4e9c9dd0
Girdled-Armadilo <yhc407@kaist.ac.kr>
2024-10-28 20:50:57 +0900

    category

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,20 +1,29 @@
-const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name | replace: " ", "_" }}: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
+const categories = { 
+  {% for category in site.categories %}
+    {% capture category_name %}{{ category | first }}{% endcapture %}
+    {{ category_name | replace: " ", "_" }}: [
+      {% for post in site.categories[category_name] %}
+        { url: "{{ site.baseurl }}{{ post.url }}", date: "{{ post.date | date_to_string }}", title: "{{ post.title }}" },
+      {% endfor %}
+    ],
+  {% endfor %}
+};
 
-console.log(categories)
+console.log(categories);
 
 window.onload = function () {
   document.querySelectorAll(".category").forEach((category) => {
     category.addEventListener("click", function (e) {
-      const posts = categories[e.target.innerText.replace(" ","_")];
-      let html = ``
-      posts.forEach(post=>{
+      const posts = categories[e.target.innerText.replace(" ", "_")];
+      let html = ``;
+      posts.forEach(post => {
         html += `
         <a class="modal-article" href="${post.url}">
           <h4>${post.title}</h4>
           <small class="modal-article-date">${post.date}</small>
         </a>
-        `
-      })
+        `;
+      });
       document.querySelector("#category-modal-title").innerText = e.target.innerText;
       document.querySelector("#category-modal-content").innerHTML = html;
       document.querySelector("#category-modal-bg").classList.toggle("open");
@@ -22,10 +31,10 @@ window.onload = function () {
     });
   });
 
-  document.querySelector("#category-modal-bg").addEventListener("click", function(){
+  document.querySelector("#category-modal-bg").addEventListener("click", function() {
     document.querySelector("#category-modal-title").innerText = "";
     document.querySelector("#category-modal-content").innerHTML = "";
     document.querySelector("#category-modal-bg").classList.toggle("open");
     document.querySelector("#category-modal").classList.toggle("open");
-  })
+  });
 };
